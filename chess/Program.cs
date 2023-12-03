344bd69cc68f0be74aef879ed7b07edf722d4b76
kondykov <kondykovs@gmail.com>
2023-12-03 22:10:18 +0300

    Namespace fixed

--- chess/Program.cs
+++ chess/Program.cs
@@ -1,4 +1,4 @@
-﻿using chess;
+﻿using Chess;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- chess/main.cs
+++ chess/main.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace chess
+namespace Chess
 {
     public partial class main : Form
     {
