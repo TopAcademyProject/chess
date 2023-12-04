8278774a46b18edb917b2a5a68abb787554ac044
kondykov <kondykovs@gmail.com>
2023-12-04 14:24:54 +0300

    FigureClass Created

---
+++ ChessClassLibrary/Figure.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessClassLibrary
+{
+    public abstract class Figure
+    {
+        private int _id;
+        private string _name;
+        public int Id { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+
+        public Figure(int id)
+        {
+            Id = id;
+        }
+    }
+    public class Pawn : Figure
+    {
+        public Pawn(int id) : base(id)
+        {
+
+        }
+    }
+}
---
+++ ChessClassLibrary/FigureList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessClassLibrary
+{
+    public class FigureList
+    {
+        public string[,] list = new string[12, 2]
+        {
+            { "11", "white pawn"},
+            { "12", ""},
+            { "13", ""},
+            { "14", ""},
+            { "15", ""},
+            { "16", ""},
+            { "21", "black pawn"},
+            { "22", ""},
+            { "23", ""},
+            { "24", ""},
+            { "25", ""},
+            { "26", ""},
+        };
+        private string[] ExtractString(string firstItem, string secondItem) => new string[2] { firstItem, secondItem };
+
+        public string[] GetFigure(int id)
+        {
+            for (int i = 0; i < 26; i++)
+            {
+                if (i == 17) i = 21;
+                if (list[i, 0] == id.ToString())
+                {
+                    return ExtractString(list[i,0], list[i,1]);
+                }
+            }
+
+            return null;
+        }
+    }
+}
