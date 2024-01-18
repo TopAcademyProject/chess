268620c7608e7bf47838c59de33f076261ef62fc
Daniil64bit <Daniil64bit.r@gmail.com>
2024-01-18 19:48:49 +0300

    Debug map Mode

--- ChessClassLibrary/ChessField/Map.cs
+++ ChessClassLibrary/ChessField/Map.cs
@@ -19,5 +19,19 @@
         public Figure GetCell(Position figurePosition) => Figures[figurePosition.X, figurePosition.Y];
 
         public Figure[,] Figures { get => figures; set => figures = value; }
+
+        //public void UpdateMap()
+
+        public Map(bool DebugMode) => Figures = new Figure[8, 8]
+    {
+                { new Figure(Player.White,5),new Figure(Player.White,4),new Figure(Player.White,3),new Figure(Player.White,2),new Figure(Player.White,1),new Figure(Player.White,3),new Figure(Player.White,4),new Figure(Player.White,5) },
+                { new Figure(Player.White,6),new Figure(Player.White,6),new Figure(Player.White,6),new Figure(Player.White,2),new Figure(Player.Empty,0),new Figure(Player.White,6),new Figure(Player.White,6),new Figure(Player.White,6) },
+                { new Figure(Player.Empty,0),new Figure(Player.Empty,0),new Figure(Player.Empty,0),new Figure(Player.Black,1),new Figure(Player.Empty,0),new Figure(Player.Empty,0),new Figure(Player.Empty,0),new Figure(Player.Empty,0) },
+                { new Figure(Player.Empty,0),new Figure(Player.Empty,0),new Figure(Player.Empty,0),new Figure(Player.Empty,0),new Figure(Player.Empty,0),new Figure(Player.Empty,0),new Figure(Player.Empty,0),new Figure(Player.Empty,0) },
+                { new Figure(Player.Empty,0),new Figure(Player.Empty,0),new Figure(Player.Empty,0),new Figure(Player.Empty,0),new Figure(Player.Empty,0),new Figure(Player.Empty,0),new Figure(Player.Empty,0),new Figure(Player.Empty,0) },
+                { new Figure(Player.Empty,0),new Figure(Player.Empty,0),new Figure(Player.Empty,0),new Figure(Player.Empty,0),new Figure(Player.Empty,0),new Figure(Player.Empty,0),new Figure(Player.Empty,0),new Figure(Player.Empty,0) },
+                { new Figure(Player.Black,6),new Figure(Player.Black,6),new Figure(Player.Black,6),new Figure(Player.Black,6),new Figure(Player.Empty,0),new Figure(Player.Black,6),new Figure(Player.Black,6),new Figure(Player.Black,6) },
+                { new Figure(Player.Black,5),new Figure(Player.Black,4),new Figure(Player.Black,3),new Figure(Player.Black,2),new Figure(Player.Black,2),new Figure(Player.Black,3),new Figure(Player.Black,4),new Figure(Player.Black,5) },
+    };
     }
 }
